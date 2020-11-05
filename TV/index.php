fa75a7e5b883e55bb521b3b288a66611985a4af1
shaoyanhan <65232041+shaoyanhan@users.noreply.github.com>
2020-11-05 19:21:38 +0800

    Add files via upload

---
+++ TV/index.php
@@ -0,0 +1,288 @@
+<?php include "header.php";?>
+
+
+	<div id="bannner" class="main_center_bannar" style="margin-top: 3%">
+        <p style="text-align: center;font-size: 2.6em">今日推荐电视剧《爱，死亡和机器人》：奈飞2019年度巨献</p>
+        <img  src="img/bk1.jpg"/>
+        <img  src="img/bk2.jpg"  style="display:none" />
+        <img  src="img/bk3.jpg"  style="display:none" />
+        <img  src="img/bk4.jpg"  style="display:none" />
+        <ul id="ulli" style="top: 28%;left: 17%">
+            <li class="selected">1</li>
+            <li class="selected">2</li>
+            <li class="selected">3</li>
+            <li class="selected">4</li> 
+        </ul>
+        
+    </div>
+
+<div  class="article" >
+    <a name="a" style="opacity: 0.01">1</a>
+    <div  class="article_left">
+        <img src="img/a.jpeg">
+    </div>
+    <div class="article_right">
+        <img src="img/a.png">
+    </div>
+</div>
+
+<div class="article" >
+    <a name="b1" style="opacity: 0.01">1</a>
+    <div  class="article_left">
+        <img src="img/b1.png">
+    </div>
+    <div class="article_right">
+        <img src="img/b1.jpeg">
+    </div>
+</div>
+
+<div  class="article" >
+    <a name="b2" style="opacity: 0.01">1</a>
+    <div  class="article_left">
+        <img src="img/b2.jpeg">
+    </div>
+    <div class="article_right">
+        <img src="img/b2.png">
+    </div>
+    </div>
+
+<div  class="article" >
+    <a name="b3" style="opacity: 0.01">1</a>
+    <div  class="article_left">
+        <img src="img/b3.png">
+    </div>
+    <div class="article_right">
+        <img src="img/b3.jpeg">
+    </div>
+    </div>
+
+<div class="article" >
+    <a name="b4" style="opacity: 0.01">1</a>
+    <div  class="article_left">
+        <img src="img/b4.jpeg">
+    </div>
+    <div class="article_right">
+        <img src="img/b4.png">
+    </div>
+    </div>
+
+<div  class="article" >
+    <a name="b5" style="opacity: 0.01">1</a>
+    <div  class="article_left">
+        <img src="img/b5.png">
+    </div>
+    <div class="article_right">
+        <img src="img/b5.jpeg">
+    </div>
+    </div>
+
+<div class="article" >
+    <a name="b6" style="opacity: 0.01">1</a>
+    <div class="article_left">
+        <img src="img/b6.jpeg">
+    </div>
+    <div class="article_right">
+        <img src="img/b6.png">
+    </div>
+    </div>
+
+<div  class="article" >
+    <a name="b7" style="opacity: 0.01">1</a>
+    <div  class="article_left">
+        <img src="img/b7.png">
+    </div>
+    <div class="article_right">
+        <img src="img/b7.jpeg">
+    </div>
+</div>
+
+<div class="article" >
+    <a name="b8" style="opacity: 0.01">1</a>
+    <div class="article_left">
+        <img src="img/b8.jpeg">
+    </div>
+    <div class="article_right">
+        <img src="img/b8.png">
+    </div>
+    </div>
+
+<div  class="article" >
+    <a name="b9" style="opacity: 0.01">1</a>
+    <div  class="article_left">
+        <img src="img/b9.png">
+    </div>
+    <div class="article_right">
+        <img src="img/b9.jpeg">
+    </div>
+</div>
+
+<div class="article" >
+    <a name="b10" style="opacity: 0.01">1</a>
+    <div class="article_left">
+        <img src="img/b10.jpeg">
+    </div>
+    <div class="article_right">
+        <img src="img/b10.png">
+    </div>
+    </div>
+
+<div  class="article" >
+    <a name="b11" style="opacity: 0.01">1</a>
+    <div  class="article_left">
+        <img src="img/b11.png">
+    </div>
+    <div class="article_right">
+        <img src="img/b11.jpeg">
+    </div>
+</div>
+
+<div class="article" >
+    <a name="c" style="opacity: 0.01">1</a>
+    <div  class="article_left">
+        <img src="img/c.jpeg">
+    </div>
+    <div class="article_right">
+        <img src="img/c.png">
+    </div>
+</div>
+
+
+
+
+
+<div class="page">
+        <div class="nav">
+            <div class="nav-li">
+                 <div class="tit"><a href="#bannner" target="_self">美图鉴赏</a></div>
+                 
+            </div>
+            <div class="nav-li">
+                 <div class="tit"><a href="#a" target="_self">剧情简介</a></div>
+                 
+            </div>
+            <div class="nav-li">
+                 <div class="tit"><a href="#b1" >分集剧情</a></div>
+                 <ul>
+                     <li>
+                         <a href="#b1" >第1～3集</a>
+                     </li>
+                     <li>
+                         <a href="#b4" >第4～6集</a>
+                     </li>
+                     <li>
+                         <a href="#b7" >第7～9集</a>
+                     </li> 
+                     <li>
+                         <a href="#b10" >第10～11集</a>
+                     </li>  
+                                                      
+                 </ul>     
+            </div>
+        
+            <div class="nav-li">
+                 <div class="tit"><a href="#c" target="_self">幕后</a></div>
+            </div>
+            <div class="nav-li">
+                 <div class="tit"><a href="#bottom" target="_self">评论区</a></div>
+            </div>
+            
+            <div class="nav-li">
+                 <div class="tit"><a href="#top" target="_self">TOP</a></div>
+                 
+            </div>                                    
+        </div>
+    </div>
+
+
+
+
+<p>&nbsp;</p>
+<p>&nbsp;</p>
+<a name="bottom"></a>
+<p style="font-size: 2em;margin-left: 45%">评论区</p>
+<p style="font-size: 1.25em;margin-left: 25%">评论请先<a href="login.php">登录</a> </p>
+<p style="font-size: 1.25em;margin-left: 25%">最近5条留言：</p>
+
+
+
+<div >
+    
+    <?php
+    $link=mysqli_connect("127.0.0.1","root","123456") or die("数据库连接失败！");
+    mysqli_select_db($link,"test") or die("数据库choose失败！<BR/>");
+    mysqli_query($link,"set names UTF8");
+    $sql="select * from tv_message1 order by sub_date DESC limit 5 ";
+    $result=mysqli_query($link,$sql); 
+
+if (!$result) {
+    printf("Error: %s\n", mysqli_error($link));
+    exit();
+}
+    while ($row=mysqli_fetch_array($result)) {
+        # code...
+      ?>
+    <div style="margin-left: 25%;margin-top:1%;width: 50%;background-color: white;opacity: 0.7">
+        <span><?php echo $row['username'] ?></span>
+        <span style="float: right;"><?php echo $row['sub_date'] ?></span>
+    </div>
+
+    <div style="margin-left: 25%;width: 50%;background-color: white;opacity: 0.9">
+        <p><?php echo $row['content'] ?></p>
+    </div>
+<?php } ?>
+</div><br/><br>
+
+	
+<?php include "tail.php";?>
+
+
+
+<script type="text/javascript">
+	var bannner = document.getElementById("bannner");
+            var pic = bannner.getElementsByTagName("img");
+            var dot = document.getElementById("ulli").getElementsByTagName("li");
+           
+            var index = 0;
+            var timer = null;
+            /*定义图片切换函数*/
+            function changePic(curIndex) {
+                for (var i = 0; i < pic.length; ++i) {
+                    pic[i].style.display = "none";
+                  
+                }
+                pic[curIndex].style.display = "block";
+                 
+            }
+            /*index超出图片总量时归零*/
+            function autoPlay() {
+                if (+index >= pic.length) {
+                    index = 0;
+                }
+                changePic(index);
+                index++;
+
+            }
+            /*定义并调用自动播放函数*/
+            timer = setInterval(autoPlay, 2000);
+            /*鼠标划过整个容器时停止自动播放*/
+            bannner.onmouseover = function () {
+                clearInterval(timer);
+            }
+            /*鼠标离开整个容器时继续播放下一张*/
+            bannner.onmouseout = function () {
+                timer = setInterval(autoPlay, 1500);
+            }
+            /*遍历所有数字导航实现划过切换至对应的图片*/
+            for (var i = 0; i < dot.length; i++) {
+                dot[i].onmouseover = function () {
+                    clearInterval(timer);
+                    index = this.innerText - 1;
+                    changePic(index)
+                   
+                }
+            }
+
+    
+</script>
+
+
